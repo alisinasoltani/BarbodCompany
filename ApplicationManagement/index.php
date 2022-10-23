6df30c2f749adb980e61ceb3a15e5a19d5a48f56
Alisina <55596860+alisinasoltani@users.noreply.github.com>
2022-10-23 08:23:53 +0330

    Some Works on Control panel

--- ApplicationManagement/index.php
+++ ApplicationManagement/index.php
@@ -1,3 +1,64 @@
-<?php
-    die('DONE');
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Control Panel</title>
+        <link rel="stylesheet" href="index.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous" />
+    </head>
+    <body class="container">
+        <div class="row">
+            <div class="col-12 header">
+                <h1 class="h6">Barbod Careers Control Panel</h1>
+            </div>
+        </div>
+        <div class="row open-positions">
+            <div class="row" style="margin: 0rem;">
+                <div class="col-12 open-positions-header">
+                    <div class="open-positions-title">
+                        <h3>Open Positions</h3>
+                    </div>
+                    <div class="open-poitions-add-btn">
+                        <button class="btn" type="button">Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin: 0rem;">
+                <div class="col-12 position">
+                    <div class="info">
+                        <div>Title</div>
+                        <div>Full Time</div>
+                        <div>Isfahan</div>
+                    </div>
+                    <button type="button" class="btn btn-outline-danger position-close-btn">
+                        close
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="row apps">
+            <div class="row app" style="margin: 0rem;">
+                <div class="col-4">
+                    Title: Title 1 / Full Time / Isfahan
+                    First Name: 
+                    Last Name:
+                    Phone Number: 09989989898
+                    Email Address: emailaddress@gmail.com
+                </div>
+                <div class="col-8">
+                    <div>
+                    Summery: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                    </div>
+                    <div>
+                        <button type="button">
+                            Download Resume
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
